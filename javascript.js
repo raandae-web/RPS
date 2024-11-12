bfb6e3c443dedcf65aaa72979325770d56a76718
raandae-web <chidoziee123@gmail.com>
2024-11-12 11:55:47 -0600

    made the human and computer functions

--- javascript.js
+++ javascript.js
@@ -1,1 +1,27 @@
+function getComputerChoice(){
+    let randomNumber = (Math.floor(Math.random()* 10));
+    if (randomNumber <= 3){
+        console.log("scissors");
+    }
+    else if (randomNumber <= 6 && randomNumber >= 3){
+        console.log("paper");
+    }
+    else {
+        console.log("rock")
+    }
+    }
 
+    
+function getHumanChoice(){
+    let answer = prompt("Rock, Paper, or Scissors?").toLowerCase();
+    if (answer == "rock" || answer == "paper" || answer == "scissors")
+    return answer;
+}
+
+let humanScore = 0
+let computerScore = 0
+
+    
+
+
+console.log(getHumanChoice())
